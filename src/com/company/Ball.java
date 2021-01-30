d204ebbc18f58e9b1887ab02cdd1b2f43874c371
mcndoungerii <allenmcndounger@gmail.com>
2021-01-30 16:40:19 +0300

    Classes

--- src/com/company/Ball.java
+++ src/com/company/Ball.java
@@ -1,11 +1,18 @@
 package com.company;
 
 public class Ball {
+
+    //All these below are the children of a class
+
+    //Properties or Instance variables
     public String color;
     public String name;
     public int bounceRate;
 
-    //Properties
+    //Behaviours
+    public void bounce() {
+        System.out.println("Bouncing ...");
+    }
     public void inflates() {
         System.out.println("Inflating ...");
     }
